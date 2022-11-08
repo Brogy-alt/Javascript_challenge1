dba671f394d909c6f8df97606a1dc491cd1ffbca
Brogy-alt <brogangys2@gmail.com>
2022-11-08 11:19:10 +0200

    Food pls

---
+++ js/code.js
@@ -0,0 +1,39 @@
+// challenge
+//my way
+
+// let userName = prompt("Please enter a name: ");
+// let userSurname = prompt("Please enter a surname: ");
+// let userSalary = prompt("Please enter salary: ");
+// let userSalarybonus = eval(userSalary) + 500;
+
+// document.write("<br>" + userName);
+// document.write("<br>" + userSurname );
+// document.write("<br>" + userSalarybonus);
+
+// console.log(userName);
+
+// console.log(userSurname);
+
+// console.log(userSalarybonus);
+
+
+
+
+
+// another way
+
+// let userFirstname = prompt("Please enter your name");
+// let userSurname = prompt("Please enter your surname");
+// let userSalary = prompt("Please enter your salary");
+// let userSalarySum = eval(userSalary) + 500;
+
+// document.write(`Name:${userFirstname}`);
+// document.write(`<br>Surame:${userSurname}`);
+// document.write(`<br>Salary:${userSalarySum}`);
+
+
+// console.log(`Name:${userFirstname}`);
+// console.log(`Surname:${userSurname}`);
+// console.log(`Salary:${userSalarySum}`);
+
+
